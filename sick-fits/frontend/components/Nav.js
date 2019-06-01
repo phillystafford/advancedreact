5cf03776e159a40bd099427e5bbc2ae0be093286
Philly Stafford <phillystafford@gmail.com>
2019-06-01 01:18:11 +0100

    Lesson 04 complete - header, nav, meta and app examples

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Nav = () => (
+  <div>
+    <Link href="/sell">
+      <a>Sell!</a>
+    </Link>
+    <Link href="/">
+      <a>home</a>
+    </Link>
+  </div>
+);
+
+export default Nav;
--- sick-fits/frontend/progress.md
+++ sick-fits/frontend/progress.md
@@ -1,5 +1,10 @@
 # Progress
 
+## Module 1 - Introduction and setup
+
 Lesson 01 - complete
 Lesson 02 - complete
 Lesson 03 - complete
+Lesson 04 - complete
+
+## Module 2 - CSS and styled components
